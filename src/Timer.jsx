d0cd819474ba98c9b1724587656131aacf89e7c1
Lionel Wirz <128322618+LionelWirz@users.noreply.github.com>
2024-02-28 22:46:09 +0000

    Circle and Timer

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -25,34 +25,31 @@ function Timer({ time, isRunning }) {
   return (
     <div className="container">
       <svg width="200" height="200" viewBox="0 0 200 200">
-      <circle
-  cx="100"
-  cy="100"
-  r="90"
-  fill="none"
-  stroke="#AED6F1" // Light blue color for the circle
-  strokeWidth="10"
-  className="remaining-circle" // Add class name for remaining time circle
-/>
-<circle
-  cx="100"
-  cy="100"
-  r="90"
-  fill="none"
-  stroke="#2C3E50" // Black background color
-  strokeWidth="10"
-  strokeDasharray="565.48" // Circumference of the circle (2 * π * radius)
-  strokeDashoffset={(100 - percentageRemaining) / 100 * 565.48} // Offset to show remaining time
-  transform="rotate(-90 100 100)"
-  className="background-circle" // Add class name for background circle
-/>
-
+        <circle
+          cx="100"
+          cy="100"
+          r="90"
+          fill="none"
+          stroke="#AED6F1" // Light blue color for the circle
+          strokeWidth="10"
+          className="remaining-circle" // Add class name for remaining time circle
+        />
+        <circle
+          cx="100"
+          cy="100"
+          r="90"
+          fill="none"
+          stroke="#2C3E50" // Black background color
+          strokeWidth="10"
+          strokeDasharray="565.48" // Circumference of the circle (2 * π * radius)
+          strokeDashoffset={(100 - percentageRemaining) / 100 * 565.48} // Offset to show remaining time
+          transform="rotate(-90 100 100)"
+          className="background-circle" // Add class name for background circle
+        />
+        <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fill="#ffffff" fontSize="40" fontFamily="Digital-7, sans-serif">
+          {formatTime(time)}
+        </text>
       </svg>
-      {/* Remaining time */}
-      <div className="timer">
-        {formatTime(time)}
-      </div>
-      {/* Controls and Settings */}
     </div>
   );
 }
